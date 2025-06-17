fe5b54528fb8cc520ff7c1dfb5593e95ad1af8d1
fedexid <142997423+fedexid@users.noreply.github.com>
2025-06-17 14:56:44 +0700

    update components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-tertiary py-4 text-center text-textPrimary">
-      <p>Student Portal | {new Date().getFullYear()} - Husein</p>
+      <p>Student Portal | {new Date().getFullYear()} - Sein</p>
     </footer>
   );
 };
